a7544d8dc1ccbfce8f3ab1ee1f3680a15a3a1c22
ärchana <archanasj12@gmail.com>
2023-01-09 09:52:35 +0530

    all changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyBodyComponent } from './my-body/my-body.component';
 import { MyHeadComponent } from './my-head/my-head.component';
+import { MyLeftComponent } from './my-left/my-left.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyHeadComponent,
-    MyBodyComponent
+    MyBodyComponent,
+    MyLeftComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-left/my-left.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-left',
+  templateUrl: './my-left.component.html',
+  styleUrls: ['./my-left.component.scss']
+})
+export class MyLeftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
