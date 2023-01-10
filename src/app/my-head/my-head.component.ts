21ea56d9c144fe2ec2d782ae50dcbc0cc0ebd59f
Yogesh Rajput <Yogesh.Rajput@meritarc.com>
2023-01-10 10:06:09 +0530

    code for input output

--- src/app/my-head/my-head.component.ts
+++ src/app/my-head/my-head.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-my-head',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-head.component.scss']
 })
 export class MyHeadComponent implements OnInit {
+
+  @Input() inputData: any;
 
   constructor() { }
 
