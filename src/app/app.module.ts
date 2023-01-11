79448fb6f7bab59f386c12c64b4cbd4c3389632a
shruti <rajeshwaris5552@gmail.com>
2023-01-11 09:09:05 +0530

    my-right added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MyBodyComponent } from './my-body/my-body.component';
 import { MyHeadComponent } from './my-head/my-head.component';
 import { MyLeftComponent } from './my-left/my-left.component';
+import { MyRightComponent } from './my-right/my-right.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyHeadComponent,
     MyBodyComponent,
-    MyLeftComponent
+    MyLeftComponent,
+    MyRightComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-right/my-right.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-right',
+  templateUrl: './my-right.component.html',
+  styleUrls: ['./my-right.component.scss']
+})
+export class MyRightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
