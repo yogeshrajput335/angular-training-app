4350aa503e3e51afc4a8939ea3e22d880c1480d6
shruti <rajeshwaris5552@gmail.com>
2023-01-15 09:53:53 +0530

    About page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { MyUsersComponent } from './my-users/my-users.component';
 import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyAboutComponent } from './my-about/my-about.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: MyDashboardComponent },
   { path: 'user', component: MyUsersComponent },
   { path: 'learn', component: MyLearnComponent },
   { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'about', component: MyAboutComponent },
+
 ];
 
 @NgModule({
